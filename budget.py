27e0ea992f55551bbd202d16fd3213f10933ef09
John Ryan <38575933+jhryan@users.noreply.github.com>
2019-11-02 12:36:19 -0500

    Add shell context

--- budget.py
+++ budget.py
@@ -1,4 +1,11 @@
 from app import app
+from app import db
+from app.models import User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80, ssl_context=('certs/cert.pem', 'certs/key.pem'))
