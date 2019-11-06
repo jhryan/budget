5e0dcdc622cebb2763a517321e78339645e4f2dc
John Ryan <38575933+jhryan@users.noreply.github.com>
2019-11-05 22:49:27 -0600

    Add Account model

--- app/models.py
+++ app/models.py
@@ -18,9 +18,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    accounts = db.relationship('Account', backref='holder', lazy=True)
     
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return f'<User {self.username}>'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -52,3 +54,15 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    holder_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Account {self.name}>'
+
--- budget.py
+++ budget.py
@@ -1,5 +1,6 @@
 from app import create_app
 from app import db
+from app.models import Account
 from app.models import User
 
 
@@ -8,7 +9,7 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'Account': Account, 'User': User}
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80, ssl_context=('certs/cert.pem', 'certs/key.pem'))
