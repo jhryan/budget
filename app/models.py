75f2470cedd736acc6c58bd9d1f49dbd45a852de
John Ryan <38575933+jhryan@users.noreply.github.com>
2019-11-09 19:59:03 -0600

    Add self-referential one-to-many relationship to account table to allow account hierarchy

--- app/models.py
+++ app/models.py
@@ -59,6 +59,8 @@ def load_user(id):
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    parent_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+    children = db.relationship('Account', backref=db.backref('parent', remote_side=[id]), lazy=True)
     type = db.Column(db.String(9), db.CheckConstraint("type IN ('Asset', 'Liability', 'Equity', 'Income', 'Expense')", name='types'), nullable=False)
     holder_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
