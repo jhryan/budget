39c44dd713c0221400119a81779cd6c376429f34
John Ryan <38575933+jhryan@users.noreply.github.com>
2019-11-10 15:23:14 -0600

    Remove most of Flask-Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ import os
 from flask import current_app
 from flask import Flask
 from flask import request
-from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -32,7 +31,6 @@ login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = 'Please log in to access this page.'
 mail = Mail()
-bootstrap = Bootstrap()
 moment = Moment()
 
 
@@ -44,7 +42,6 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
-    bootstrap.init_app(app)
     moment.init_app(app)
 
     from app.errors import bp as errors_bp
