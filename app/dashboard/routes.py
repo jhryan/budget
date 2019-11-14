9333307dcf5ea5f26d0e1599e18cf5e4709ff20b
John Ryan <38575933+jhryan@users.noreply.github.com>
2019-11-13 20:18:38 -0600

    Extract common functionality out to authenticate function

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -14,10 +14,7 @@ from app.models import Budget
 from app.models import User
 
 
-@bp.route('/<username>/budget/', defaults={'budget_id': None})
-@bp.route('/<username>/budget/<int:budget_id>')
-@login_required
-def budget(username, budget_id):
+def authenticate(username, budget_id):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash(f'User {username} not found.')
@@ -36,52 +33,26 @@ def budget(username, budget_id):
         db.session.commit()
     
     accounts = Account.query.filter_by(budget=budget).all()
-    return render_template('dashboard/budget.html', title='Dashboard', budget=budget, user=user, accounts=accounts)
+    return user, budget, accounts
+    
+
+@bp.route('/<username>/budget/', defaults={'budget_id': None})
+@bp.route('/<username>/budget/<int:budget_id>')
+@login_required
+def budget(username, budget_id):
+    user, budget, accounts = authenticate(username, budget_id)
+    return render_template('dashboard/budget.html', title='Dashboard', user=user, budget=budget, accounts=accounts)
 
 
 @bp.route('/<username>/budget/<int:budget_id>/reports')
 @login_required
 def reports(username, budget_id):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        flash(f'User {username} not found.')
-        return redirect(url_for('main.index'))
-    if user != current_user:
-        abort(401)
-
-    if budget_id is None:
-        if user.last_budget is None:
-            budget = Budget.query.filter_by(user=user).first_or_404()
-        else:
-            budget = user.last_budget
-    else:
-        budget = Budget.query.filter_by(user=user).filter_by(id=budget_id).first_or_404()
-        user.last_budget = budget
-        db.session.commit()
-    
-    accounts = Account.query.filter_by(budget=budget).all()
-    return render_template('dashboard/reports.html', title='Dashboard', budget=budget, user=user, accounts=accounts)
+    user, budget, accounts = authenticate(username, budget_id)
+    return render_template('dashboard/reports.html', title='Dashboard', user=user, budget=budget, accounts=accounts)
 
 
 @bp.route('/<username>/budget/<int:budget_id>/accounts')
 @login_required
 def accounts(username, budget_id):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        flash(f'User {username} not found.')
-        return redirect(url_for('main.index'))
-    if user != current_user:
-        abort(401)
-
-    if budget_id is None:
-        if user.last_budget is None:
-            budget = Budget.query.filter_by(user=user).first_or_404()
-        else:
-            budget = user.last_budget
-    else:
-        budget = Budget.query.filter_by(user=user).filter_by(id=budget_id).first_or_404()
-        user.last_budget = budget
-        db.session.commit()
-    
-    accounts = Account.query.filter_by(budget=budget).all()
-    return render_template('dashboard/accounts.html', title='Dashboard', budget=budget, user=user, accounts=accounts)
+    user, budget, accounts = authenticate(username, budget_id)
+    return render_template('dashboard/accounts.html', title='Dashboard', user=user, budget=budget, accounts=accounts)
