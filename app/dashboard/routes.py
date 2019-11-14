2d47aed415b2cefac3ec200ebc280e8693624e8c
John Ryan <38575933+jhryan@users.noreply.github.com>
2019-11-14 00:27:24 -0600

    Add url_value_preprocessor and url_defaults to dashboard blueprint
    Since things like username and budget_id will be used heavily throughout the blueprint,
    this essentially scrapes them out of the url the first time they are entered and saves them off into
    g which can then be used for providing url defaults when calling url_for.
    
    Still have to be careful and provide defaults for things like budget_id when not entered - such as the last
    visited budget or the first budget a user created as a fallback.

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 from flask import abort
+from flask import current_app
+from flask import g
 from flask import redirect
 from flask import render_template
 from flask import url_for
@@ -12,6 +14,28 @@ from app.dashboard import bp
 from app.models import Account
 from app.models import Budget
 from app.models import User
+
+
+@bp.url_defaults
+def add_url_defaults(endpoint, values):
+    if 'username' not in values and g.username:
+        if current_app.url_map.is_endpoint_expecting(endpoint, 'username'):
+            values['username'] = g.username
+    if 'budget_id' not in values and g.budget_id:
+        if current_app.url_map.is_endpoint_expecting(endpoint, 'budget_id'):
+            values['budget_id'] = g.budget_id
+
+
+@bp.url_value_preprocessor
+def pull_url_values(endpoint, values):
+    g.username = values.pop('username', None)
+    g.budget_id = values.pop('budget_id', None)
+
+    if g.username is None:
+        g.username = current_user.username
+    if g.budget_id is None:
+        last_budget_id = User.query.filter_by(username=g.username).first().last_budget_id
+        g.budget_id = last_budget_id if last_budget_id else Budget.query.join(Budget, User.budgets).first()
 
 
 def authenticate(username, budget_id):
@@ -36,20 +60,20 @@ def authenticate(username, budget_id):
 @bp.route('/<username>/budget/', defaults={'budget_id': None})
 @bp.route('/<username>/budget/<int:budget_id>')
 @login_required
-def budget(username, budget_id):
-    user, budget, accounts = authenticate(username, budget_id)
+def budget():
+    user, budget, accounts = authenticate(g.username, g.budget_id)
     return render_template('dashboard/budget.html', title='Dashboard', user=user, budget=budget, accounts=accounts)
 
 
 @bp.route('/<username>/budget/<int:budget_id>/reports')
 @login_required
-def reports(username, budget_id):
-    user, budget, accounts = authenticate(username, budget_id)
+def reports():
+    user, budget, accounts = authenticate(g.username, g.budget_id)
     return render_template('dashboard/reports.html', title='Dashboard', user=user, budget=budget, accounts=accounts)
 
 
 @bp.route('/<username>/budget/<int:budget_id>/accounts')
 @login_required
-def accounts(username, budget_id):
-    user, budget, accounts = authenticate(username, budget_id)
+def accounts():
+    user, budget, accounts = authenticate(g.username, g.budget_id)
     return render_template('dashboard/accounts.html', title='Dashboard', user=user, budget=budget, accounts=accounts)
